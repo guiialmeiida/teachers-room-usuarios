8969888af47555c51cfb4bbc2ae728bb238cb15f
guiialmeiida <gui.almeidacrz@gmail.com>
2020-06-13 19:47:18 -0300

    Alterando a inicialização do servidor

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+require('./src/controllers/authController')(app);
+require('./src/controllers/projectController')(app);
+require('./src/controllers/UserController')(app);
+require('./src/controllers/AulaController')(app);
+require('./src/controllers/turmaController')(app);
+
+app.listen(3000, () =>{
+    console.log('Servidor iniciado na porta 3000: http://localhost:3000/');
+});
